08f0fc13cc845bd2183aaeffb57a8f1944af7495
Kate <test@gmail.com>
2020-12-11 00:51:40 -0500

    CaesarCipher encrypt two keys]

--- prerequisite/03.Java-Programming-Arrays-lists-data/code/StringSample/src/CaesarCipher.java
+++ prerequisite/03.Java-Programming-Arrays-lists-data/code/StringSample/src/CaesarCipher.java
@@ -1,6 +1,6 @@
 /**
  * The source code for the first week homework CaesarCipher.
- * */
+ */
 
 public class CaesarCipher {
     protected final static String alphabetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -8,8 +8,9 @@ public class CaesarCipher {
 
     public static void main(String[] args) {
         CaesarCipher example = new CaesarCipher();
-        String content = "Hello, world";
-        example.encrypt(content, 97);
+        String content = "At noon be in the conference room with your hat on for a surprise party. YELL LOUD!";
+        example.encrypt(content, 15);
+        example.encryptTwoKeys(content, 8, 21);
     }
 
     /**
@@ -44,6 +45,41 @@ public class CaesarCipher {
                 + result.toString() + "\n");
     }
 
+    public String encryptTwoKeys(String input, int key1, int key2) {
+        StringBuilder outputSb = new StringBuilder(input);
+        String key1UpperLetters = this.getShiftedAlphabet(key1, false);
+        String key1LowerLetters = this.getShiftedAlphabet(key1, true);
+        String key2UpperLetters = this.getShiftedAlphabet(key2, false);
+        String key2LowerLetters = this.getShiftedAlphabet(key2, true);
+
+        int curIndex = 0;
+        int inputLen = input.length();
+
+        while (curIndex < inputLen) {
+            char curLetter = outputSb.charAt(curIndex);
+            char newLetter = curLetter;
+            boolean isLowerCase = Character.isLowerCase(curLetter);
+            boolean isEvenIndex = curIndex % 2 == 0;
+            int newIndex = isLowerCase ? alphabetLowerCase.indexOf(curLetter) : alphabetUpperCase.indexOf(curLetter);
+            if (newIndex != -1) {
+                if (isEvenIndex) {
+                    newLetter = isLowerCase ? key1LowerLetters.charAt(newIndex) : key1UpperLetters.charAt(newIndex);
+                } else {
+                    newLetter = isLowerCase ? key2LowerLetters.charAt(newIndex) : key2UpperLetters.charAt(newIndex);
+                }
+            }
+            outputSb.setCharAt(curIndex, newLetter);
+            curIndex += 1;
+        }
+
+
+        System.out.println("For source =" + input
+                + "\n, after two keys, the output is \n"
+                + outputSb.toString());
+
+        return outputSb.toString();
+    }
+
 
     /**
      * Given a key, get the shifted alphabet
@@ -53,10 +89,11 @@ public class CaesarCipher {
      */
     public String getShiftedAlphabet(int key, boolean useLowerCase) {
         int len = alphabetLowerCase.length();
-        int validKey = key % len;
+        int validKey = (key + len) % len;
         if (useLowerCase) {
             return alphabetLowerCase.substring(validKey) + alphabetLowerCase.substring(0, validKey);
         }
         return alphabetUpperCase.substring(validKey) + alphabetUpperCase.substring(0, validKey);
     }
+
 }
